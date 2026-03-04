5bc71db9df35baf2de42de405d061a0ab77573aa
p0675892-jpg <p0675892@gmail.com>
2026-03-04 15:14:45 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/theme.css";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+// ✅ Register the service worker
+serviceWorkerRegistration.register();
