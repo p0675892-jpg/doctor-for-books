60a97508b6d54de580c50b6fe83e93b819e66f25
princesss <p0675892@gmail.com>
2026-03-15 05:14:42 +0000

    update

--- src/index.js
+++ src/index.js
@@ -3,22 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/theme.css";
 
-// ------------------- Service Worker Registration -------------------
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then((reg) => {
-        console.log("Service Worker registered successfully:", reg);
-      })
-      .catch((err) => {
-        console.log("Service Worker registration failed:", err);
-      });
-  });
-}
-
 // ------------------- Render React App -------------------
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
